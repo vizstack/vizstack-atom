7a0893ba694293619d6540a53174c3f6464ae4d4
Nikhil Bhattasali <nikhilxb@stanford.edu>
2019-06-22 14:59:26 -0700

    Refactor of package name

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup
 
 setup(
-    name='vizstack-atom',
-    packages=['vizstack'],
-    zip_safe=False,
+    name='vz-atom',
+    packages=['vzatom'],
 )
---
+++ python/vzatom/__init__.py
@@ -0,0 +1,1 @@
+from vzatom.atom import *
